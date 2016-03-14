e06a9ad74c46ea9061d0ecf20d7026a80c083e6b
Gajendra D Ambi <gajuambi@gmail.com>
2016-03-14 23:06:50 +0530

    14/3/2016
    14/3/2016

---
+++ vTool/vToolMenus/MainMenu/vCenterMenu/HostMenu/CoreDump.ps1
@@ -0,0 +1,38 @@
+﻿
+function CoreDump {
+<#
+.SYNOPSIS
+    configure Coredump on esxi hosts
+.DESCRIPTION
+    This will check the version of the esxi and based on the version of it, it will set the coredump settings on the host
+.NOTES
+    File Name      : CoreDump.ps1
+    Author         : gajendra d ambi
+    Date           : January 2016
+    recommended    : PowerShell v4+, powercli 6+ over win7 and upper.
+    Copyright      - None
+.LINK
+    Script posted over: github.com/gajuambi/vmware   
+#>
+
+#start of the script
+#start of the function
+
+$DumpTarget = Read-Host "Type the DumpTarget?:"
+$vmk        = Read-Host "Type the vmk number?:"
+ foreach ($vmhost in (get-vmhost | sort)) {
+  if ((get-vmhost $vmhost).version -match 5.) {
+  $esxcli = get-vmhost $VMHost | Get-EsxCli
+  $esxcli.system.coredump.network.set($null, $vmk , $DumpTarget , "6500")
+  $esxcli.system.coredump.network.set("true")
+  $esxcli.system.coredump.network.get()
+  }
+
+  if ((get-vmhost $vmhost).version -match 6.) {
+  $esxcli = get-vmhost $vmhost | get-esxcli
+  $esxcli.system.coredump.network.set($null , $vmk , $null , $DumpTarget , "6500")
+  $esxcli.system.coredump.network.set("true")
+  $esxcli.system.coredump.network.get()
+  }
+ }#end of the function
+}#end of the script
---
+++ vTool/vToolMenus/MainMenu/vCenterMenu/HostMenu/HostServicesMenu/SetDCUI.ps1
@@ -0,0 +1,37 @@
+﻿#start of function
+function SetDCUI 
+{
+<#
+.SYNOPSIS
+    Configure DCUI [Direct Console UI]
+.DESCRIPTION
+    Depending upon the choice that you make
+    This will enable DCUI.
+    This will disable DCUI.
+    This will set DCUI policy to On which will be persistent across reboot.
+    This will set DCUI policy to Off which will be persistent across reboot.
+.NOTES
+    File Name      : SetDCUI.ps1
+    Author         : gajendra d ambi
+    Date           : March 2016
+    Prerequisite   : PowerShell v4+, powercli 6+ over win7 and upper.
+    Copyright      - None
+.LINK
+    Script posted over: github.com/gajuambi/vmware
+#>
+#Start of script#
+$cluster = Read-Host "name of the cluster[type * to include all clusters]?"
+Write-Host "DCUI [Direct Console UI] options
+     1. Enable DCUI
+     2. Disable DCUI
+     3. DCUI Policy On
+     4. DCUI Policy Off
+     " -BackgroundColor White -ForegroundColor Blue #options to choose from
+ $a = Read-Host "Choose a number from 1 to 4"
+ if ($option -eq 1) {get-cluster $cluster | get-vmhost | get-vmhostservice | where Key -EQ DCUI | Start-VMHostService}
+ if ($option -eq 2) {get-cluster $cluster | get-vmhost | get-vmhostservice | where Key -EQ DCUI | stop-VMHostService}
+ if ($option -eq 3) {get-cluster $cluster | get-vmhost | get-vmhostservice | where Key -EQ DCUI | Set-VMHostService -Policy On}
+ if ($option -eq 4) {get-cluster $cluster | get-vmhost | get-vmhostservice | where Key -EQ DCUI | Set-VMHostService -Policy Off}
+    
+ #End of Script#
+}#End of function
