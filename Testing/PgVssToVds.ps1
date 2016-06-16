35ccb7fcddbd01266853083a8a3ebcc8cafde371
Gajendra D Ambi <gajuambi@gmail.com>
2016-06-16 07:19:37 +0530

    vxrack

---
+++ Testing/PgVssToVds.ps1
@@ -0,0 +1,31 @@
+﻿#start of function
+function PgVssToVds
+{
+<#
+.SYNOPSIS
+    Migrate Portgroup from Vss to Vds.
+.DESCRIPTION
+    This will .
+.NOTES
+    File Name      : PgVssToVds.ps1
+    Author         : gajendra d ambi
+    Date           : June 2016
+    Prerequisite   : PowerShell v4+, powercli 6+ over win7 and upper.
+    Copyright      - None
+.LINK
+    Script posted over: VCE Internal
+#>
+Write-Host "this will work on only those hosts which are part of the VDS" -BackgroundColor Black -ForegroundColor White
+$vds = Read-Host "name of the vds?"
+$pg  = Read-Host "name of the source portgroup on vss?" 
+$dpg = Read-Host "name of the destination portgroup on vds?"
+
+$dpg = Get-VDSwitch -Name 
+$DvsHosts = Get-VDSwitch -Name $vds | get-vmhost | sort
+foreach ($vmhost in $DvsHosts)
+    {
+     $vmk = Get-VMHostNetworkAdapter -VMHost $vmhost | where PortgroupName -eq $pg
+     Set-VMHostNetworkAdapter -PortGroup $dpg -VirtualNic $vmk -Confirm:$false
+     Get-VMHost $vmhost | Get-VirtualPortGroup -Standard -Name $pg | Remove-VirtualPortGroup -Confirm:$false
+    }
+}#End of function
