5f82c69303a253bd4bf1498963977c0c5cf499bf
Gajendra D Ambi <gajuambi@gmail.com>
2017-03-02 10:25:51 +0530

    vtool march 2017

--- vTool/vToolMenus/MainMenu/vCenterMenu/VssMenu/L3VmotionGateway.ps1
+++ vTool/vToolMenus/MainMenu/vCenterMenu/VssMenu/L3VmotionGateway.ps1
@@ -1,4 +1,6 @@
-
+#Start of function L3VmotionGateway
+function L3VmotionGateway
+{
 <#
 .SYNOPSIS
     Gets the plink
@@ -13,52 +15,13 @@
 .LINK
     Script posted over: 
     github.com/mrambig
-    [source] http://www.virtu-al.net/2013/01/07/ssh-powershell-tricks-with-plink-exe/
 #>
 #Start of function
-function GetPlink 
-{
-<#
-.SYNOPSIS
-    Gets the plink
-.DESCRIPTION
-    This will make sure plink is either downloaded from the internet if it is not present and if it cannot download
-    then it will pause the script till you copy it manually.
-.NOTES
-    File Name      : GetPlink.ps1
-    Author         : gajendra d ambi
-    Date           : August 2016
-    Prerequisite   : PowerShell v4+, over win7 and upper.
-    Copyright      - None
-.LINK
-    Script posted over: 
-    github.com/mrambig
-    [source] http://www.virtu-al.net/2013/01/07/ssh-powershell-tricks-with-plink-exe/
-
-#>
-$PlinkLocation = $PSScriptRoot + "\Plink.exe"
-$presence = Test-Path $PlinkLocation
-if (-not $presence) 
-    {
-    Write-Host "Missing Plink.exe, trying to download...(10 seconds)" -BackgroundColor White -ForegroundColor Black
-    Invoke-RestMethod "http://the.earth.li/~sgtatham/putty/latest/x86/plink.exe" -TimeoutSec 10 -OutFile "plink.exe"
-    if (-not $presence)
-        {
-            do
-            {
-            Write-Host "Unable to download plink.exe, please download and add it to the same folder as this script" -BackgroundColor Yellow -ForegroundColor Black
-            Read-host "Hit Enter/Return once plink is present"
-            $presence = Test-Path $PlinkLocation
-            } while (-not $presence)
-        }
-    }
-
-if ($presence) { Write-Host "Detected Plink.exe" -BackgroundColor White -ForegroundColor Black }
-} #End of function
-
 GetPlink
 #If using in powershell then add snapins below for VMware ESXi.
 Add-PSSnapin VMware.VimAutomation.Core -ErrorAction SilentlyContinue
+
+Write-Host "This will only work if you can dns resolve hostnames from this machine"
 
 #X server's credentials
 $user = Read-Host "Host's username?"
@@ -81,3 +44,4 @@ get-cluster $cluster | Get-VMHost $VMHost | Get-VMHostService | where {$_.Key -e
 }
 
 write-host "Reboot hosts (vtool->host>power options) to make sure the changes are visible" -ForegroundColor yellow
+} #End of function L3VmotionGateway
---
+++ vTool/vToolMenus/MainMenu/vCenterMenu/VssMenu/shootVmk.ps1
@@ -0,0 +1,30 @@
+#start of function shootVmk
+function shootVmk 
+{
+<#
+.SYNOPSIS
+    delete vmkernel
+.DESCRIPTION
+    This will remove the vmkernel from hosts. you can do this for standard or virtual portgroup and then delete the portgroup.
+.NOTES
+    File Name      : shootVmk.ps1
+    Author         : gajendra d ambi
+    Date           : march 2017
+    Prerequisite   : PowerShell v4+, powercli 6+ over win7 and upper.
+    Copyright      - None
+.LINK
+    Script posted over: github.com/gajuambi/vmware
+#>
+#Start of script#
+$cluster = Read-Host "Name of the Cluster?"
+$vmk = Read-Host "name of the vmk? (ex:vmk4)"
+$vmhosts = get-cluster $cluster | get-vmhost
+
+foreach ($vmhost in $vmhosts)
+{
+$vmk = Get-VMHostNetworkAdapter -VMHost $vmhost | where DeviceName -eq $vmk
+$vmk
+Remove-VMHostNetworkAdapter -Nic $vmk -confirm:$false
+}
+} #end of function shootVmk
+shootVmk
