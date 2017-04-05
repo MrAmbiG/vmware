0e4bc57a403b537060a595d8b24e44ab137d34ee
Gajendra D Ambi <gajuambi@gmail.com>
2017-04-05 11:38:45 +0530

    vmdiskinfo

---
+++ vmDiskOSInfo.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+    Get linux vms and their disk info
+.DESCRIPTION
+    Gets the VMs and filters them by linux OS, get their disk count and their sizes.    
+.NOTES
+    File Name      : vmDiskOSInfo.ps1
+    Author         : gajendra d ambi
+    Date           : April 2017
+    Prerequisite   : PowerShell v4+, powercli 6+ over win7 and upper.
+    Copyright      - None
+.LINK
+    Script posted over: github.com/MrAmbiG/vmware
+#>
+$vms = get-vm | where { $_.ExtensionData.Guest.GuestFamily -match 'linuxGuest' }
+$system = New-Object -TypeName PSObject
+foreach ($vm in $vms)
+{
+New-Object PSObject -Property @{
+            'VM Name' = $vm.name
+            'OS' = $vm.Guest.OSFullName
+            'DiskCount' = ($vm | Get-HardDisk).count
+            'DiskSizes' = ($vm | Get-HardDisk).CapacityGB -join ';'
+             }
+}
