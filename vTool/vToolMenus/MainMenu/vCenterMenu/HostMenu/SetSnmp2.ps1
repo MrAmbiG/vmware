4771d56b29fd84f26a4d571493d27473f2479f18
sneha kamath <sneha.kamath@vce.com>
2017-08-22 16:54:45 +0530

    updating VMkernel, PowerMgmt, Setsnmp,VssVmkPg2.ps1

---
+++ vTool/vToolMenus/MainMenu/vCenterMenu/HostMenu/SetSnmp2.ps1
@@ -0,0 +1,40 @@
+﻿#start of function
+function SetSnmp 
+{
+<#
+.SYNOPSIS
+    Configure SNMP
+.DESCRIPTION
+    This will configure SNMP using powercli on esxi hosts. It uses esxcli commands into powercli.
+.NOTES
+    File Name      : SetSnmp.ps1
+    Author         : gajendra d ambi
+    Date           : March 2016
+    Prerequisite   : PowerShell v4+, powercli 6+ over win7 and upper.
+    Copyright      - None
+.LINK
+    Script posted over: github.com/gajuambi/vmware
+#>
+#Start of script#
+$cluster = Read-Host "name of the cluster[type * to include all clusters]?"
+$snmp    = Read-Host "Type the snmp target"
+$string  = Read-Host "Type the snmp communities string"
+
+$stopWatch = [system.diagnostics.stopwatch]::startNew()
+$stopWatch.Start()
+
+  foreach ($vmhost in (get-cluster $cluster | get-vmhost | sort)) {
+        $esxcli = get-vmhost $vmhost | get-esxcli -v2
+        $esxcliset = $esxcli.system.snmp.set
+        $args = $esxcliset.CreateArgs()
+        $args.communities = "$string"
+        $args.enable = "true"
+        $args.targets = "$snmp"
+        $esxcli.Invoke($args)
+  
+  }
+
+$stopWatch.Stop()
+Write-Host "Elapsed Runtime:" $stopWatch.Elapsed.Hours "Hours" $stopWatch.Elapsed.Minutes "minutes and" $stopWatch.Elapsed.Seconds "seconds." -BackgroundColor White -ForegroundColor Black
+#End of Script#
+}#End of function
---
+++ vTool/vToolMenus/MainMenu/vCenterMenu/VssMenu/VssVmkPg2.ps1
@@ -0,0 +1,65 @@
+﻿#start of function
+Function VssVmkPg 
+{
+<#
+.SYNOPSIS
+    update vmkernel portgroup on vSwitch.
+.DESCRIPTION
+    This will update vmkernel portgroup on a chosen standard vSwitch of hosts of a chosen cluster.    
+.NOTES
+    File Name      : VssVmkPg.ps1
+    Author         : gajendra d ambi
+    Date           : March 2016
+    last update    : March 2017[added mtu option]
+    Prerequisite   : PowerShell v4+, powercli 6+ over win7 and upper.
+    Copyright      - None
+.LINK
+    Script posted over: github.com/gajuambi/vmware
+#>
+#Start of Script
+$cluster = Read-Host "name of the cluster[type * to include all clusters]?"
+$vss     = Read-Host "name of the vSphere standard Switch?"
+$pg      = Read-Host "Name of the portgroup?"
+$vmk     = Read-Host "vmk number? ex:- vmk9"
+$mtu     = Read-Host "mtu?"
+$ip      = Read-Host "starting ip address?" 
+$mask    = Read-Host "subnet mask"
+$vlan    = Read-Host "Vlan?"
+
+$stopWatch = [system.diagnostics.stopwatch]::startNew()
+$stopWatch.Start()
+
+$a       = $ip.Split('.')[0..2]
+   
+  #first 3 octets of the ip address
+  $b     = [string]::Join(".",$a)
+  
+  #last octet of the ip address
+  $c     = $ip.Split('.')[3]
+  $c     = [int]$c
+
+  foreach ($vmhost in (get-cluster $cluster | get-vmhost | sort)) {
+  $vmhost.name
+  get-vmhost $vmhost | get-virtualswitch -Name $vss | New-VirtualPortGroup -Name $pg -VLanId $vlan -Confirm:$false
+        $esxcli = get-vmhost $vmhost | get-esxcli -v2
+        $esxcliset1 = $esxcli.network.ip.interface.add 
+        $args1 = $esxcliset1.CreateArgs()
+        $args1.interfacename = "$vmk"
+        $args1.mtu = "$mtu"
+        $args1.portgroupname = "$pg"
+        $esxcliset1.Invoke($args1)
+        $esxcliset2 = $esxcli.network.ip.interface.ipv4.set 
+        $args2 = $esxcliset2.CreateArgs()
+        $args2.interefacename = "$vmk"
+        $args2.type = "static"
+        $args2.ipv4 = "$b.$(($c++))"
+        $args2.netmask = "$mask"
+        $esxcliset2.Invoke($args2)
+ }
+
+$stopWatch.Stop()
+Write-Host "Elapsed Runtime:" $stopWatch.Elapsed.Hours "Hours" $stopWatch.Elapsed.Minutes "minutes and" $stopWatch.Elapsed.Seconds "seconds." -BackgroundColor White -ForegroundColor Black
+ #End of Script#
+}#End of function
+VssVmkPg
+
